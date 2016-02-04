6d2435320cfa54332d140cd61e69def58538a34d
Terry Tai <poshboytl@gmail.com>
2016-02-04 15:42:46 +0800

    Added missed fonts.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,15 @@
 //= require lib/Chart.min
 //= require lib/toastr.min
 //= require_tree .
+
+window.app = angular.module('Nevermore', ['neverMoreFilters']);
+
+angular.element(document).ready(function() {
+  // window.credit_book = CreditBook.deployed();
+  // window.order_book = OrderBook.deployed();
+  // window.currentUser = localStorage.getItem("currentUser") || web3.eth.accounts[0];
+  // order_book.setCreditBook(CreditBook.deployed_address, {from: currentUser});
+
+  // Bootstrap Angualr module
+  angular.bootstrap(document, ['Nevermore']);
+});
